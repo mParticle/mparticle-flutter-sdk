8d5366fda21689207c4473319c2da5545f407e63
Robert Ing <ring@mparticle.com>
2021-09-20 21:07:50 -0400

    Update lib/identity/identity_api_error_response.dart

--- lib/identity/identity_api_error_response.dart
+++ lib/identity/identity_api_error_response.dart
@@ -2,7 +2,7 @@ import 'package:mparticle_flutter_sdk/identity/client_error_codes.dart';
 
 /// A class that returns when an identity request fails for some reason.
 ///
-//  This is passed to the handler for both client and server errors.
+///  This is passed to the handler for both client and server errors.
 class IdentityAPIErrorResponse {
   int? httpCode;
   String? mpid;
